c4d15360724d3f71ce7ec408c7fd6d4b8048da7a
Justin Etheredge <justin@etheredge.us>
2015-04-02 17:07:14 -0400

    Increase default stretches to 12
    The current default number of stretches for devise should probably be increased from 10 to at least 12.

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -100,7 +100,7 @@ Devise.setup do |config|
   # a value less than 10 in other environments. Note that, for bcrypt (the default
   # encryptor), the cost increases exponentially with the number of stretches (e.g.
   # a value of 20 is already extremely slow: approx. 60 seconds for 1 calculation).
-  config.stretches = Rails.env.test? ? 1 : 10
+  config.stretches = Rails.env.test? ? 1 : 12
 
   # Setup a pepper to generate the encrypted password.
   # config.pepper = '<%= SecureRandom.hex(64) %>'
